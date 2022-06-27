5fad2fbf3368b134f8b6eb8cd8693939ff3ebf67
webertmaciel <webert_maciel@hotmail.com>
2022-06-26 22:28:29 -0300

    App

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes , Route } from 'react-router-dom'
+import Event from './pages/Event'
+import Subscribe from './pages/Subscribe'
+
+const Router = () => {
+  return (
+   <Routes>
+    <Route path="/" element={<Subscribe />}/>
+    <Route path="/event" element={<Event/>}/>
+    <Route path="/event/lesson/:slug" element={<Event/>}/>
+   </Routes>
+  )
+}
+
+export default Router
